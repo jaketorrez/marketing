c7ec3760cb3f7d0dbcee33c07bac6c10d0893129
Jake Torres <jake@jaketorres.dev>
2020-06-26 08:24:09 -0600

    Last touches; desktop layout in place.

---
+++ scripts/nav_menu_mobile.js
@@ -0,0 +1,11 @@
+const toggle = document.querySelector('#toggle');
+const menu = document.querySelector('#menu');
+toggle.addEventListener('click', function() {
+    if (menu.classList.contains('is-active')) {
+        this.setAttribute('aria-expanded', 'false');
+        menu.classList.remove('is-active');
+    } else {
+        menu.classList.add('is-active');
+        this.setAttribute('aria-expanded', 'true');
+    }
+});
